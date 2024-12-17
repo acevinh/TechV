c904d783248eb3ab03fb29b19dcacc69328e54e7
vnvtn8@gmail.com <vnvtn8@gmail.com>
2024-12-17 19:37:13 +0700

    layout v1 by Vinh

---
+++ resources/views/clients/blocks/banner.blade.php
@@ -0,0 +1,45 @@
+<div class="slide-home-04">
+    <div class="response-product product-list-owl owl-slick equal-container better-height"
+         data-slick="{&quot;arrows&quot;:true,&quot;slidesMargin&quot;:0,&quot;dots&quot;:false,&quot;infinite&quot;:false,&quot;speed&quot;:300,&quot;slidesToShow&quot;:1,&quot;rows&quot;:1}"
+         data-responsive="[{&quot;breakpoint&quot;:480,&quot;settings&quot;:{&quot;slidesToShow&quot;:1,&quot;slidesMargin&quot;:&quot;0&quot;}},{&quot;breakpoint&quot;:768,&quot;settings&quot;:{&quot;slidesToShow&quot;:1,&quot;slidesMargin&quot;:&quot;0&quot;}},{&quot;breakpoint&quot;:992,&quot;settings&quot;:{&quot;slidesToShow&quot;:1,&quot;slidesMargin&quot;:&quot;0&quot;}},{&quot;breakpoint&quot;:1200,&quot;settings&quot;:{&quot;slidesToShow&quot;:1,&quot;slidesMargin&quot;:&quot;0&quot;}},{&quot;breakpoint&quot;:1500,&quot;settings&quot;:{&quot;slidesToShow&quot;:1,&quot;slidesMargin&quot;:&quot;0&quot;}}]">
+        <div class="slide-wrap">
+            <img src="assets/images/slide41.jpg" alt="image">
+            <div class="slide-info">
+                <div class="container">
+                    <div class="slide-inner">
+                        <h1>New Collection</h1>
+                        <h5>2019-2020</h5>
+                        <h2>Sale Up <span>35%</span> Off</h2>
+                        <a href="#">Shop now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="slide-wrap">
+            <img src="assets/images/slide42.jpg" alt="image">
+            <div class="slide-info">
+                <div class="container">
+                    <div class="slide-inner">
+                        <h1>Deal Of Day</h1>
+                        <h5>Hot Speaker</h5>
+                        <h2>Get Extra <span>25%</span> Off</h2>
+                        <a href="#">Shop now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="slide-wrap">
+            <img src="assets/images/slide43.jpg" alt="image">
+            <div class="slide-info">
+                <div class="container">
+                    <div class="slide-inner">
+                        <h1>Discovery Now</h1>
+                        <h5>Best Selling</h5>
+                        <h2>Sale Flat <span>60%</span> Off</h2>
+                        <a href="#">Shop now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/clients/blocks/footer.blade.php
@@ -0,0 +1,208 @@
+<footer id="footer" class="footer style-04">
+    <div class="section-001 section-009">
+        <div class="container">
+            <div class="kobolg-newsletter style-01">
+                <div class="newsletter-inner">
+                    <div class="newsletter-info">
+                        <div class="newsletter-wrap">
+                            <h3 class="title">Newsletter</h3>
+                            <h4 class="subtitle">Get Discount 30% Off</h4>
+                            <p class="desc">Suspendisse netus proin eleifend fusce sollicitudin potenti vel magnis
+                                nascetur</p>
+                        </div>
+                    </div>
+                    <div class="newsletter-form-wrap">
+                        <div class="newsletter-form-inner">
+                            <input class="email email-newsletter" name="email" placeholder="Enter your email ..."
+                                   type="email">
+                            <a href="#" class="button btn-submit submit-newsletter">
+                                <span class="text">Subscribe</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section-025">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-lg-12 d-lg-none">
+                    <div class="logo-footer">
+                        <img src="assets/images/logo.png"
+                             class="az_single_image-img attachment-full" alt="img">
+                    </div>
+                    <div class="footer-desc">Kobolg helps you sell anything.<br/>
+                        The best choice for your next personal
+                    </div>
+                    <div class="kobolg-socials style-01">
+                        <div class="content-socials">
+                            <ul class="socials-list">
+                                <li>
+                                    <a href="https://facebook.com/" target="_blank">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank">
+                                        <i class="fa fa-instagram"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/" target="_blank">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.pinterest.com/" target="_blank">
+                                        <i class="fa fa-pinterest-p"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="kobolg-listitem style-04">
+                        <div class="listitem-inner">
+                            <h4 class="title">Customer </h4>
+                            <ul class="listitem-list">
+                                <li>
+                                    <a href="#" target=" _blank">
+                                        Shipping &amp; Returns </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_self">
+                                        Secure Shopping </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_self">
+                                        Order Status </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_self">
+                                        International Shipping </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 d-none d-lg-block">
+                    <div class="logo-footer">
+                        <img src="assets/images/logo.png"
+                             class="az_single_image-img attachment-full" alt="img">
+                    </div>
+                    <div class="footer-desc">Kobolg helps you sell anything.<br/>
+                        The best choice for your next personal
+                    </div>
+                    <div class="kobolg-socials style-01">
+                        <div class="content-socials">
+                            <ul class="socials-list">
+                                <li>
+                                    <a href="https://facebook.com/" target="_blank">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank">
+                                        <i class="fa fa-instagram"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/" target="_blank">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.pinterest.com/" target="_blank">
+                                        <i class="fa fa-pinterest-p"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="kobolg-listitem style-04">
+                        <div class="listitem-inner">
+                            <h4 class="title">
+                                Information </h4>
+                            <ul class="listitem-list">
+                                <li>
+                                    <a href="#" target="_self">
+                                        Privacy Policy </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_self">
+                                        Customer Service </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_self">
+                                        Orders and Returns </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_self">
+                                        Contact </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section-016">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>© Copyright 2020 <a href="#">Kobolg</a>. All Rights Reserved.</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="payment">
+                        <img src="assets/images/payment.png"
+                             class="az_single_image-img attachment-full" alt="img">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="footer-device-mobile">
+    <div class="wapper">
+        <div class="footer-device-mobile-item device-home">
+            <a href="index.html">
+					<span class="icon">
+						<i class="fa fa-home" aria-hidden="true"></i>
+					</span>
+                Home
+            </a>
+        </div>
+        <div class="footer-device-mobile-item device-home device-wishlist">
+            <a href="wishlist.html">
+					<span class="icon">
+						<i class="fa fa-heart" aria-hidden="true"></i>
+					</span>
+                Wishlist
+            </a>
+        </div>
+        <div class="footer-device-mobile-item device-home device-cart">
+            <a href="cart.html">
+					<span class="icon">
+						<i class="fa fa-shopping-basket" aria-hidden="true"></i>
+						<span class="count-icon">
+							0
+						</span>
+					</span>
+                <span class="text">Cart</span>
+            </a>
+        </div>
+        <div class="footer-device-mobile-item device-home device-user">
+            <a href="my-account.html">
+					<span class="icon">
+						<i class="fa fa-user" aria-hidden="true"></i>
+					</span>
+                Account
+            </a>
+        </div>
+    </div>
+</div>
